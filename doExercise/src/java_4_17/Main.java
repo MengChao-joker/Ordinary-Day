cd5a59258de6504ca1236f0af3875463d0797188
MengChao-joker <1307949473@qq.com>
2021-04-18 23:55:16 +0800

    update

--- doExercise/src/java_4_17/Main.java
+++ doExercise/src/java_4_17/Main.java
@@ -1,15 +1,51 @@
 package java_4_17;
 
+import java.util.Scanner;
+
 public class Main {
 
-    //判断回文
-    public static Boolean isHW(String str){
-
-
-        return true;
-    }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()){
+            int a = scanner.nextInt();
+            int[] nums = new int[a];
+            for (int i = 0; i < a; i++) {
+                nums[i]=scanner.nextInt();
+            }
+            int sum = 0; int max = nums[0];
+            for (int i = 0; i < a; i++) {
+                sum+=nums[i];
+                max = max>sum?max:sum;
+                if(sum<0){
+                    sum = 0;
+                }
 
+            }
+        }
+    }
+
+    public static void main1(String[] args) {
+        String s = "asd";
+        StringBuffer s1 = new StringBuffer("asdf");
+        System.out.println(s.substring(0,1));
+        System.out.println(s1.substring(0,0));
+
+    }
+    //判断回文
+    public static void main2(String[] args) {
+        Scanner s = new Scanner(System.in);
+        while (s.hasNext()) {
+            String str1 = s.nextLine();
+            String str2 = s.nextLine();
+            int res = 0;
+            String s1 = "";
+            for (int i = 0; i < str1.length(); i++) {
+                s1 = str1.substring(0, i) + str2 + str1.substring(i, str1.length());
+                StringBuilder stringBuilder = new StringBuilder(s1);
+                if (stringBuilder.reverse().toString().equals(s1)) res++;
+            }
+            System.out.println(res);
+        }
     }
 }
--- doExercise/src/java_4_17/Test.java
+++ doExercise/src/java_4_17/Test.java
@@ -3,6 +3,23 @@ package java_4_17;
 
 import java.util.HashMap;
 
+class T {
+    T(String s){
+        System.out.println(s);
+    }
+
+    public static void hello() {
+        System.out.println("hello");
+    }
+}
+class Y extends T{
+
+    Y(String s) {
+        super(s);
+        System.out.println(s);
+    }
+}
+
 class TreeNode {
     int val;
     TreeNode left;
@@ -22,26 +39,50 @@ class TreeNode {
     }
 }
 
-public class Test {
 
-    public static void main(String[] args) {
-        int x,y;
-        x = 5>>>2;
-        y = x>>>2;
+public class Test {
+    static class Fun{
+        int a = 0;  }
+    public static int a = 0;
+
+    public static void main(String[] args) throws Exception {
+        if(true) throw new Exception("error");
+    }
+    public static void main3(String[] args) {
+        Fun a = new Fun();
+        Fun b= a;
+        b.a = 12;
+        System.out.println(a.a);
+
+
+    }
+    public static void main2(String[] args) {
+// TODO Auto-generated method stub
+        T t = null;
+        t.hello();
+    }
+
+
+    public static void main1(String[] args) {
+        int x, y;
+        x = 5 >>> 2;
+        y = x >>> 2;
         System.out.println(y);
         System.out.println(x);
     }
+
     //二叉搜索树不同节点间的最小差值
     //思路：中序遍历二叉搜索树，求得最小差值
     int res = Integer.MAX_VALUE;
     int pre = -1;
-    public int minDisTree(TreeNode root){
-        if(root==null) return 0;
+
+    public int minDisTree(TreeNode root) {
+        if (root == null) return 0;
         minDisTree(root.left);
-        if(pre==-1){
+        if (pre == -1) {
             pre = root.val;
-        }else {
-            res = Math.min(res, root.val-pre);
+        } else {
+            res = Math.min(res, root.val - pre);
             pre = root.val;
         }
         minDisTree(root.right);
