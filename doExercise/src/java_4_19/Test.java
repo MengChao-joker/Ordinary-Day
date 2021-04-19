120f7c4869aafd6f4a8fe215a1e041318a4cd840
MengChao-joker <1307949473@qq.com>
2021-04-20 01:15:41 +0800

    update

---
+++ doExercise/src/java_4_19/Test.java
@@ -0,0 +1,19 @@
+package java_4_19;
+
+public class Test {
+    private static int tem = 12;
+    public static class Test1{
+
+    }
+    public static void main(String[] args) {
+        Test test = new Test();
+//        test.tem; //实例对象不能访问静态变量
+    }
+
+
+    public static void main1(String[] args) {
+        System.out.println(Math.round(11.6));
+        System.out.println(Math.round(11.5));
+        System.out.println(Math.round(-11.7));
+    }
+}
