e1235d31740e2aa18345cbca938f569dbff9e4c7
MengChao-joker <1307949473@qq.com>
2021-06-06 16:23:44 +0800

    a

--- doExercise/src/June/java_6_6/Solution.java
+++ doExercise/src/June/java_6_6/Solution.java
@@ -1,8 +1,6 @@
 package June.java_6_6;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
 
 
 class CQueue {
@@ -30,6 +28,28 @@ class CQueue {
     }
 }
 public class Solution {
+    //1030. 距离顺序排列矩阵单元格
+    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+        int[][] re = new int[R * C][2];
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                int t = i*C+j;
+                re[t][0] = i;
+                re[t][1] = j;
+            }
+        }
+        Arrays.sort(re, (arr1, arr2) -> {
+            int d1 = dist(arr1[0], arr1[1], r0, c0);
+            int d2 = dist(arr2[0], arr2[1], r0, c0);
+            return Integer.compare(d1, d2);
+        });
+
+        return re;
+    }
+
+    private int dist(int r1,int c1,int r2,int c2) {
+        return Math.abs(r1 - r2) + Math.abs(c1 - c2);
+    }
     //1732. 找到最高海拔
     public int largestAltitude(int[] gain) {
         int ret = 0;
@@ -52,4 +72,5 @@ public class Solution {
         return true;
 
     }
+
 }
